0d4d48605c623c55506a76c15635f22881369b6f
Unknown <mattimatti2000@gmail.com>
2018-09-12 14:47:37 +0200

    table promise

--- todo.js
+++ todo.js
@@ -1,5 +1,6 @@
 const readline = require('readline');
 const sqlite3 = require('sqlite3').verbose();
+let db;
 
 //readline interface create
 const rl = readline.createInterface({
@@ -11,18 +12,42 @@ const rl = readline.createInterface({
 
 //promise to create tododatabase
 var createDbPromise = new Promise(function (resolve, reject) {
-    let TodoDb = new sqlite3.Database('todo.db', (err) => {
+    db = new sqlite3.Database('todo.db', (err) => {
         if (err){
             reject('error on creating todo database: ' + err);
         }
         else {
-            resolve('Todo DB created succesfully');
+            resolve('Todo DB created successfully');
+
         }
     });
 });
 
 createDbPromise.then(function (fromResolve) {
     console.log(fromResolve);
+    test();
 }).catch(function (fromReject) {
     console.log(fromReject);
-})
+});
+
+///promise to create table if not exist
+function test() {
+
+    var createTablePromise = new Promise(function (resolve, reject) {
+        db.run("CREATE TABLE if not exists test (vote TEXT)", [], (err) => {
+            if (err){
+                reject('Error on creating table' + err);
+            }
+            else {
+                resolve('OK table')
+            }
+        });
+    });
+
+    createTablePromise.then(function (fromResolve) {
+        console.log(fromResolve);
+    }).catch(function (fromReject) {
+        console.log(fromReject);
+    });
+
+}
