c490a1117b0200706bcff3d393daeda7fc004b60
Unknown <mattimatti2000@gmail.com>
2018-10-06 20:02:59 +0200

    ability to add todo

--- expressapp.js
+++ expressapp.js
@@ -4,13 +4,20 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const port = 3000;
 
+var todoList = [
+    "todo 1",
+    "todo 2",
+    "Homework",
+    "test123"
+]
+
 //view engine:
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 //body parser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 //set static path
@@ -23,7 +30,20 @@ app.get('/', function(req, res){
 
 // to-do page
 app.get('/todo', function(req, res){
-    res.render('todo');
+    res.render('todo', {todoList: todoList});
+});
+
+// submit route
+app.post("/newtodo", function(req, res) {
+    console.log("item submitted");
+    var item = req.body.item;
+    todoList.push(item);
+    res.redirect("/todo");
+})
+
+//catch non valid routes
+app.get('*', function (req, res) {
+    res.send("<h1>This page does not exist</h1>");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
