ec53a09ce531b39f8f9cec3db11b7d759495d37d
Unknown <mattimatti2000@gmail.com>
2018-10-07 17:22:25 +0200

    database

--- expressapp.js
+++ expressapp.js
@@ -3,13 +3,27 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const port = 3000;
+const mysql = require('mysql');
+var test;
 
-var todoList = [
-    "todo 1",
-    "todo 2",
-    "Homework",
-    "test123"
-]
+var todoList = [];
+var finishedList =[];
+
+// connect to the database
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "app"
+})
+
+//check database connection
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected to the database!");
+})
+
+
 
 //view engine:
 app.set('view engine', 'ejs');
@@ -30,7 +44,33 @@ app.get('/', function(req, res){
 
 // to-do page
 app.get('/todo', function(req, res){
-    res.render('todo', {todoList: todoList});
+    //get data from the database
+    //get the not yet done to-do's
+    con.query("select * from todo WHERE done = 0", function (err, rows) {
+        if (err){
+            console.log(err);
+            return;
+        }
+        rows.forEach(function(result) {
+            console.log(result.todo, result.done);
+            todoList.push(result.todo);
+        });
+        con.query("select * from todo WHERE done = 1", function(err, rows) {
+            if (err){
+                console.log(err);
+                return;
+            }
+            rows.forEach(function(result) {
+                finishedList.push(result.todo);
+                console.log(result.todo, result.done);
+            })
+            res.render('todo', {todoList: todoList, finishedList: finishedList});
+            todoList = [];
+            finishedList = [];
+        })
+
+    });
+
 });
 
 // submit route
@@ -38,7 +78,16 @@ app.post("/newtodo", function(req, res) {
     console.log("item submitted");
     var item = req.body.item;
     todoList.push(item);
-    res.redirect("/todo");
+    //inserted the to-do in the database
+    con.query("INSERT INTO todo (todo, done) VALUES ('"+item+"', 0)", function(err, result) {
+        if (err){
+            console.log(err);
+            return;
+        }
+        console.log("inserted: ", item);
+        res.redirect("/todo");
+    })
+
 })
 
 //catch non valid routes
